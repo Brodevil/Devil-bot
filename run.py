41213021a538ff7d9209ca339ea8ce14f9028a29
Brodevil <brodevil89@gmail.com>
2021-07-05 12:38:01 +0530

    Initial commit

--- run.py
+++ run.py
@@ -11,13 +11,13 @@ from src.constants import Client
 
 log = logging.getLogger('discord')
 log.setLevel(logging.DEBUG)
-handler = log.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 log.addHandler(handler)
 
 
 # extensions loading
-with open("src\\resource\\extensions\\_cog.json") as _cog:
+with open("src\\resource\\extensions\\_cogs.json") as _cog:
     _cog = json.load(_cog)
     _cog = list(_cog["cogs"])
 
