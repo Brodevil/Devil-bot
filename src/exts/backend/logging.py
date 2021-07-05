2bfe830e34a224f7e16a5aed73bc1d85a61c7613
Brodevil <brodevil89@gmail.com>
2021-07-05 16:37:42 +0530

    Upadted work

--- src/exts/backend/logging.py
+++ src/exts/backend/logging.py
@@ -3,7 +3,7 @@ import logging
 from discord.ext import commands
 from discord import Embed
 
-from src.constants import Channels
+from src.constants import Channels              # noqa
 
 
 log = logging.getLogger(__name__)
@@ -14,9 +14,9 @@ class Logging(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        
+        self.bot.run_task(self.startup_log)
 
-    @self.bot.event()
+
     async def startup_log(self):
         log.info("Bot connected!")
 
@@ -28,6 +28,7 @@ class Logging(commands.Cog):
 
         log_channel = self.bot.get_channel(Channels.LOG_CHANNEL)
         await log_channel.send(embed=embed)
+    
 
 
 def setup(bot: commands.Bot):
