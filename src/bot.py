c82bd1e4ce75dffdfacdfecb356bb5464b593d72
Brodevil <brodevil89@gmail.com>
2021-07-09 20:05:24 +0530

    See the commit

--- src/bot.py
+++ src/bot.py
@@ -26,7 +26,7 @@ class Bot(commands.Bot):
         super().__init__(**kwargs)
         self.statuses = ["The Bot is Currently under the Development by Brodevil#0001", "Hey!", "To kese hain aap log", "RONIT#8477 is Best", 
                         "Nothing special, Just under Development :)", "PLAYGING A GAME", "Author : Brodevil#0001", f'Over {len(set(super().get_all_members()))} users!', 
-                        f'Over {len(super().guilds)} guilds!','Forgot your prefix? @mention me!', 'over your mind']
+                        f'Over {len(super().servers)} guilds!','Forgot your prefix? @mention me!', 'over your mind']
         
         self.change_status.start()
 
@@ -42,7 +42,7 @@ class Bot(commands.Bot):
         for _ in self.statuses:
 
             await super().change_presence(status=discord.Status.idle, activity=discord.Game(_), )
-            
+
         
 
 
