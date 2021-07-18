c4650d21066bcd70a269e0dd9b185e4fabcc9db9
Brodevil <brodevil89@gmail.com>
2021-07-18 13:05:47 +0530

    Just printing the messages

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import json
 
 from src.bot import bot             # noqa
 from src.constants import Client     # noqa
-
+from discord.ext import command     # noqa
 
 
 log = logging.getLogger('discord')
@@ -11,6 +11,16 @@ log.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 log.addHandler(handler)
+
+@bot.event()
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    
+    elif message.guild:
+        print(f"Author : {message.author} \nServer :{message.guild.name} \nMessage : {message.content}\n\n")
+    else:
+        print(f"DM User : {message.author} \nMessage : {message.content}\n\n")
 
 
 # extensions loading
