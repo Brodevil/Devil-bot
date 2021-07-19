9034497fc7171226d35b72dca492bd5dfc0f24c1
Brodevil <brodevil89@gmail.com>
2021-07-19 20:42:22 +0530

    Still wanna make the command to reload a cog

---
+++ src/exts/moderation/owner/reload_cog.py
@@ -0,0 +1,24 @@
+from typing import Optional, Literal
+
+import logging
+
+from discord.ext.commands import Bot
+from discord.ext.commands import Cog
+from discord.ext import commands
+
+__all__ = ("Reload_cog", "setup")
+
+class Relod_cogs(Cog):
+    def __init__(self, bot: Bot):
+        self.bot = bot
+
+    
+    @commands.is_owner()
+    @commands.command(name="reload", aliases=("reload_cog", "recog", "cog"))
+    async def cog_reloader(self, ctx: commands.Context, ):
+        pass
+
+
+def setup(bot: Bot):
+    bot.add_cog(Relod_cogs(bot))
+
