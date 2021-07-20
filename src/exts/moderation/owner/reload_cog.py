a16768b4e17b96f0270bc2a20d9e208d592f5231
Brodevil <brodevil89@gmail.com>
2021-07-20 19:32:20 +0530

    dekh le bhai

--- src/exts/moderation/owner/reload_cog.py
+++ src/exts/moderation/owner/reload_cog.py
@@ -1,12 +1,20 @@
 from typing import Optional, Literal
 
 import logging
+import json 
 
 from discord.ext.commands import Bot
 from discord.ext.commands import Cog
 from discord.ext import commands
 
+from discord import Embed
+
+from src.constants import Colours
+
+
+logger = logging.getLogger(__name__)
 __all__ = ("Reload_cog", "setup")
+
 
 class Reload_cogs(Cog):
     def __init__(self, bot: Bot):
@@ -15,9 +23,24 @@ class Reload_cogs(Cog):
     
     @commands.is_owner()
     @commands.command(name="reload", aliases=("reload_cog", "recog", "cog"))
-    async def cog_reloader(self, ctx: commands.Context, ):
-        pass
+    async def cog_reloader(self, ctx: commands.Context, cog: Optional[str] = None):
+        """Reloads the cogs """
 
+        with open("src\\resource\\extensions\\status.json") as f:
+            f = json.load(f)
+            f = list(f["cogs"])
+                
+        if cog is None:
+            self.bot.loading_extensions(extensions=f, reload=True)
+
+        elif cog in f:
+            self.bot.load_extension(reload=True, extension=cog)
+        
+        else:
+            embed = Embed(color=Colours.soft_red)
+            embed.add_field(name=""
+            
+        
 
 def setup(bot: Bot):
     bot.add_cog(Reload_cogs(bot))
