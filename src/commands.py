ce55c2ae01a8e2d6923f809b959ac11f6fdd3726
Brodevil <brodevil89@gmail.com>
2021-07-22 20:54:49 +0530

    practicing of reloading cogs

--- src/commands.py
+++ src/commands.py
@@ -18,12 +18,9 @@ class Commands(commands.Cog):
     @commands.command(name="hello", aliases=("hey", "hlo", "test"))
     async def hello_world(self, ctx: commands.Context):
         await ctx.message.add_reaction("👋")
-        embed = discord.Embed(color=Colours.soft_red)
-        embed.set_image(url="https://media.tenor.com/images/7e6a7b73faa414e321811e0ecb34519e/tenor.gif")
-        await ctx.send(embed=embed)
-
-
-
+        # embed = discord.Embed(color=Colours.soft_red)
+        # embed.set_image(url="https://media.tenor.com/images/7e6a7b73faa414e321811e0ecb34519e/tenor.gif")
+        # await ctx.send(embed=embed)
 
 def setup(bot: commands.Bot):
     bot.add_cog(Commands(bot))
