055ccab177e66ff2eb1f2cf29c46bddf2f59842e
Brodevil <brodevil89@gmail.com>
2021-07-23 21:02:23 +0530

    file full of discord decorators

--- src/exts/utils/decorators.py
+++ src/exts/utils/decorators.py
@@ -2,3 +2,27 @@ import asyncio
 
 import discord
 from discord.exts import commands
+
+
+async def confirm_action(ctx: commands.Context, bot_command: commands.Command):
+        await ctx.message.add_reaction("✅")
+        await ctx.message.add_reaction("❌")
+        
+        def check(reaction, user):
+            if user == ctx.message.author:
+                if str(reaction.emoji) == '✅' or str(reaction.emoji) == "❌":
+                    return True
+        
+        try:
+            reaction, user = await self.bot.wait_for('reaction_add', timeout=30.0, check=check)
+        except asyncio.TimeoutError:
+            embed = Embed(title="🚫 Action Cancled!", color=constants.Colours.soft_red)
+            await ctx.send(embed=embed)
+            return 
+        
+        if str(reaction) == "❌":
+            embed = Embed(title="🚫 Action Cancled!", color=constants.Colours.soft_red)
+            await ctx.send(embed=embed)
+            return
+        
+        await bot_command()
