ec43958d2a89a4a6c092953501cc57f74ff98069
Brodevil <brodevil89@gmail.com>
2021-07-23 21:00:16 +0530

    bot

--- src/bot.py
+++ src/bot.py
@@ -54,30 +54,6 @@ class Bot(commands.Bot):
         else:   
             self.reload_extension(single_cog)
         
-    
-    async def confirm_command(self, ctx: commands.Context, bot_command: commands.Command):
-        await ctx.message.add_reaction("✅")
-        await ctx.message.add_reaction("❌")
-        
-        def check(reaction, user):
-            if user == ctx.message.author:
-                if str(reaction.emoji) == '✅' or str(reaction.emoji) == "❌":
-                    return True
-        
-        try:
-            reaction, user = await self.bot.wait_for('reaction_add', timeout=30.0, check=check)
-        except asyncio.TimeoutError:
-            embed = Embed(title="🚫 Action Cancled!", color=constants.Colours.soft_red)
-            await ctx.send(embed=embed)
-            return 
-        
-        if str(reaction) == "❌":
-            embed = Embed(title="🚫 Action Cancled!", color=constants.Colours.soft_red)
-            await ctx.send(embed=embed)
-            return
-        
-        await bot_command()
-
 
 
 with open("src\\resource\\extensions\\status.json") as _activies:
