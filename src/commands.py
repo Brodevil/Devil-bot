660f83fbf4919748414ff2f644bac99c2d5c3977
Brodevil <brodevil89@gmail.com>
2021-07-25 20:17:22 +0530

    just for practicing

--- src/commands.py
+++ src/commands.py
@@ -1,9 +1,11 @@
+import asyncio
 import logging
 
 from discord.ext import commands
 import discord
 
 from src.constants import Colours
+from src.exts.utils.converter import acute_remover
 
 
 log = logging.getLogger(__name__)
@@ -18,9 +20,15 @@ class Commands(commands.Cog):
     @commands.command(name="hello", aliases=("hey", "hlo", "test"))
     async def hello_world(self, ctx: commands.Context):
         await ctx.message.add_reaction("👋")
-        # embed = discord.Embed(color=Colours.soft_red)
-        # embed.set_image(url="https://media.tenor.com/images/7e6a7b73faa414e321811e0ecb34519e/tenor.gif")
-        # await ctx.send(embed=embed)
+
+
+    @commands.is_owner()
+    @commands.command(name="eval", aliases=("e", ))
+    async def eval_cog(self, ctx: commands.Contextm, code):
+        code = acute_remover(code)
+        output = eval(code)
+        ctx.reply(f"```{output}```")
+
 
 def setup(bot: commands.Bot):
     bot.add_cog(Commands(bot))
