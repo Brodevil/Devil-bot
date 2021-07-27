3022ac4429b402ef825967a187af8eeb5e0a02ee
Brodevil <brodevil89@gmail.com>
2021-07-27 20:18:38 +0530

    Updated file

--- src/context.py
+++ src/context.py
@@ -1,21 +1,43 @@
 import asyncio
+import logging
 
 from discord.ext.commands import Context
-import discord
+from discord import Embed
+from discord.ext import commands
 
-from src.bot import bot
-from src.exts.utils.decorators import confirm_action
 from src.exts.utils.exceptions import ActionCancle
 
+from src.constants import Colours
+from src.bot import bot
 
+
+__all__ = ("NewContext", )
+
+logger = logging.getLogger(__name__)
+
+    
 class NewContext(Context):
     """My New Coustum Context"""
     
     async def confirm_action(self):
         """Confirming the Action"""
-        if await confirm_action(ctx=self) is None:
-            raise ActionCancle("Anction Cancled")
-        
-        else:
-            pass
+        await self.message.add_reaction("✅") 
+        await self.message.add_reaction("❌")
+
+        def check(reaction, user):
+            if user == self.message.author:
+                if str(reaction.emoji) == '✅' or str(reaction.emoji) == "❌":
+                    return True
+
+        try:
+            reaction, user = await bot.wait_for('reaction_add', timeout=30.0, check=check)
+        except asyncio.TimeoutError:
+            embed = Embed(title="🚫 Action Cancled!", color=Colours.soft_red)
+            await self.send(embed=embed)
+            raise ActionCancle("Time Out! So Action Cancled!")
+
+        if str(reaction) == "❌":
+            embed = Embed(title="🚫 Action Cancled!", color=Colours.soft_red)
+            await self.send(embed=embed)
+            raise ActionCancle("Action Cancle by the user!")
 
