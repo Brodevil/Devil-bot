ce5338ea2db06d19f7fce4ef010e772d926e2026
Brodevil <brodevil89@gmail.com>
2021-07-28 19:39:23 +0530

    Welcoming the user

--- src/exts/backend/welcome.py
+++ src/exts/backend/welcome.py
@@ -1,11 +1,18 @@
 import logging
 
-from discord.ext import commands
+from discord.ext.commands import Cog
+from discord.ext.commands import Bot
 import discord
 
+log = logging.getLogger(__name__)
 
-class Welcome(commands.Cog):
-    def __init__(self, bot: commands.Bot):
+
+class Welcome(Cog):
+    def __init__(self, bot: Bot):
         self.bot = bot
 
 
+
+
+def setup(bot: Bot):
+    bot.add_cog(Welcome(bot))
