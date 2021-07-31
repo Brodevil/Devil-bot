f0350219e464b3138b3bdbe3b6f45c3ccdcae22f
Brodevil <brodevil89@gmail.com>
2021-07-31 20:02:47 +0530

    removed confirm message

--- src/exts/utils/checkers.py
+++ src/exts/utils/checkers.py
@@ -1,32 +1,6 @@
 import discord
-import asyncio
 
-from src.bot import bot
-
-__all__ = ("confirm_action", "random_num_check")
-
-
-async def confirm_action(ctx):
-    """Confirming the Action"""
-    await ctx.message.add_reaction("✅") 
-    await ctx.message.add_reaction("❌")
-
-    def check(reaction, user):
-        if user == ctx.message.author:
-            if str(reaction.emoji) == '✅' or str(reaction.emoji) == "❌":
-                return True
-
-    try:
-        reaction, user = await bot.wait_for('reaction_add', timeout=30.0, check=check)
-    except asyncio.TimeoutError:
-        return False
-
-    if str(reaction) == "❌":
-        return False
-    
-    else:
-        return True
-
+__all__ = ("random_num_check")
 
 def random_num_check(author: discord.User):
     def inner_check(message):
