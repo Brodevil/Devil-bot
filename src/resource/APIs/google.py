5cab1fa9ca87d094df21d9bca539dcfff0c0ecb7
Brodevil <brodevil89@gmail.com>
2021-08-18 21:15:26 +0530

    its happened with requests

--- src/resource/APIs/google.py
+++ src/resource/APIs/google.py
@@ -2,6 +2,7 @@ import aiohttp
 from aiohttp import ClientSession, ClientResponse
 import asyncio
 
+import requests
 from bs4 import BeautifulSoup
 
 
@@ -9,16 +10,20 @@ async def short_google_search(queary: str) -> str:
     """Short Google Search"""
     queary = "-".join(queary.split())
     url = f"https://www.google.com/search?q={queary}"
-    print(url)
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as data:
-            data = await data.text()
-            print(data)
-            data = BeautifulSoup(data, "html.parser")
-            data = data.find('div', class_="BNeawe")
-            # print(data)
-    
+
+    # async with aiohttp.ClientSession() as session:
+    #     async with session.get(url) as data:
+    #         data = await data.text()
+    #         print(data)
+    #         data = BeautifulSoup(data, "html.parser")
+    #         data = data.find('div', class_="BNeawe")
+    #         # print(data)
+    data = requests.get(url)
+    print()
+    data = BeautifulSoup(data.text, "html.parser")
+    data = data.find("div", class_="BNeawe").text
+
     return data
 
 
