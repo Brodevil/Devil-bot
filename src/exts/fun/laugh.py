1f1230f283365a7a0a1b23b3727e7d74202a5dcb
Brodevil <brodevil89@gmail.com>
2021-08-21 19:44:07 +0530

    Intial Commit

---
+++ src/exts/fun/laugh.py
@@ -0,0 +1,22 @@
+from logging import getLogger
+from typing import AnyStr
+
+import discord
+from discord.ext.commands import Context, Bot, Cog, command
+from discord.ext import commands
+
+from src.constants import Colours
+
+log = getLogger(__name__)
+
+
+class Laugh(Cog):
+    def __init__(self, bot: Bot) -> None:
+        self.bot = bot
+    
+    @command(name="joke", aliases=("text_joke", "small_joke", ))
+    async def shortJokes(self, ctx: Context):
+        pass
+
+def setup(bot: Bot) -> None:
+    bot.add_cog(Laugh(bot))
