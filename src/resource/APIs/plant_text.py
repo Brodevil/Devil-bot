9e585b1a629d077e334812fea9049efd2deed372
Brodevil <brodevil89@gmail.com>
2021-08-25 20:02:55 +0530

    aiohttp

--- src/resource/APIs/plant_text.py
+++ src/resource/APIs/plant_text.py
@@ -1,9 +1,10 @@
-from typing import getLogger
+from logging import getLogger
 
 import asyncio
 import aiohttp
-import json
+from json import load
 
+__all__= ("get_quote", )
 log = getLogger(__name__)
 
 
@@ -11,7 +12,7 @@ async def get_quote() -> str:
     url = "https://zenquotes.io/api/random"
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as quote:
-            quote = await quote.json()
-            quote = quote[0]["q"] + " -" + quote[0]["a"]
-
-    return quote
+            quote = await quote.text()
+            print(quote)
+            quote = load(quote)
+            return quote[0]["q"], quote[0]["a"]
