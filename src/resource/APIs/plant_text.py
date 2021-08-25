c246a4f0910591a23535fa83b3434996e58e3a32
Brodevil <brodevil89@gmail.com>
2021-08-25 19:53:42 +0530

    function to get the quote in plan text format

--- src/resource/APIs/plant_text.py
+++ src/resource/APIs/plant_text.py
@@ -1,2 +1,17 @@
+from typing import getLogger
+
 import asyncio
 import aiohttp
+import json
+
+log = getLogger(__name__)
+
+
+async def get_quote() -> str:
+    url = "https://zenquotes.io/api/random"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as quote:
+            quote = await quote.json()
+            quote = quote[0]["q"] + " -" + quote[0]["a"]
+
+    return quote
