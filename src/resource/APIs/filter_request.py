5a0d79f8580b74c306f301651f5a83fb76e58061
Brodevil <brodevil89@gmail.com>
2021-08-29 20:47:05 +0530

    till now work

--- src/resource/APIs/filter_request.py
+++ src/resource/APIs/filter_request.py
@@ -11,7 +11,7 @@ async def short_google_search(queary: str) -> str:
     """Short Google Search"""
     queary = "-".join(queary.split())
     url = f"https://www.google.com/search?q={queary}"
-    
+
     data = requests.get(url)
     data = BeautifulSoup(data.text, "html.parser")
     data = data.find("div", class_="BNeawe").text
@@ -25,9 +25,15 @@ async def qna_jokes():
     
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as joke:
-            joke = await joke.content()
+            joke = await joke.text()
             joke = BeautifulSoup(joke, "html5lib")
-            question = joke.find('p', calss_="question")
-            answer = joke.find('p', class_="answer")
-            print(question, answer)
+            question = joke.find('p', attrs = {'class': "question"})
+            answer = joke.find('p', attrs = {'class': "answer"})
+            question = question.split("</b>")[-1].replace("</p>", ""))
+            answer = answer.split("</b>")[-1].replace("</p>", ""))
             return question, answer
+
+
+if __name__ == "__main__":
+    run = asyncio.get_event_loop()
+    print(run.run_until_complete(qna_jokes()))
