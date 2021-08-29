6fe88abf2d140abe83cebac5a13542f38889dd0f
Brodevil <brodevil89@gmail.com>
2021-08-29 20:14:24 +0530

    store the till here work or code

--- src/resource/APIs/filter_request.py
+++ src/resource/APIs/filter_request.py
@@ -4,24 +4,30 @@ import asyncio
 
 import requests
 from bs4 import BeautifulSoup
+from random import randint
 
 
 async def short_google_search(queary: str) -> str:
     """Short Google Search"""
     queary = "-".join(queary.split())
     url = f"https://www.google.com/search?q={queary}"
-
-
-    # async with aiohttp.ClientSession() as session:
-    #     async with session.get(url) as data:
-    #         data = await data.text()
-    #         data = BeautifulSoup(data, "html.parser")
-    #         data = data.find('div', class_="BNeawe")
-    #         print(data)
-
+    
     data = requests.get(url)
     data = BeautifulSoup(data.text, "html.parser")
     data = data.find("div", class_="BNeawe").text
 
     return data
 
+
+async def qna_jokes():
+    """Get the random QNA Jokes"""
+    url=r"https://readme-jokes.vercel.app/api?bgColor=%23696969&textColor=%23FFC0CB&aColor=%23AFEEEE&borderColor=%23F5F5F5"
+    
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as joke:
+            joke = await joke.content()
+            joke = BeautifulSoup(joke, "html5lib")
+            question = joke.find('p', calss_="question")
+            answer = joke.find('p', class_="answer")
+            print(question, answer)
+            return question, answer
