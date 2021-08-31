5be2b683fc2c14ff25cdf3a126bfbc0a8f9b209a
Brodevil <brodevil89@gmail.com>
2021-08-31 19:59:33 +0530

    will be creating ban comamnd soon

--- src/exts/moderation/volient_action.py
+++ src/exts/moderation/volient_action.py
@@ -1,7 +1,11 @@
 from logging import getLogger
 
 from discord.ext.commands import Bot, Context, command, Cog
+from discord.ext import commands
 from discord import Embed
+import discord
+
+from src.constants import Colours
 
 
 log = getLogger(__name__)
@@ -11,6 +15,10 @@ class VolientAction(Cog):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
     
+    @commands.has_permissions()
+    @command(name="ban", aliases=("ban_user", ))
+    async def ban(self, ctx: Context, user: discord.User, time: ):
+        pass
 
 
 def setup(bot: Bot) -> None:
