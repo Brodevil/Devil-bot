0253f5fb1087411ce46f6243f2ab8c46e21c05f2
Brodevil <brodevil89@gmail.com>
2021-09-06 19:55:27 +0530

    new command, uptime

---
+++ src/exts/info/bot_info.py
@@ -0,0 +1,43 @@
+import logging
+import time
+
+import discord
+from discord import Embed
+from discord.ext.commands import Bot, Cog, Context, command
+from src.constants import Colours
+
+starttime = time.time()
+logging.getLogger(__name__)
+
+
+class Bot_info(Cog):
+    """Few Commands for Bot informations"""
+
+    def __init__(self, bot: Bot):
+        self.bot = bot
+    
+
+    @command(name="ping")
+    async def ping(self, ctx: Context):
+        """Ping of Bot in miliseconds"""
+        start_time = time.time()
+        message = await ctx.send("Testing Ping...")
+        end_time = time.time()
+
+        embed = Embed(title="Pong!", color=Colours.soft_red)
+        
+        embed.add_field(name=f"Gateway Latency : ", value=f"{round(self.bot.latency * 1000)} ms", inline=False)
+        embed.add_field(name=f"Discord API latency :", value=f"{str(end_time - start_time)[2:5]} ms", inline=False)
+        embed.set_footer(text=f"Requested by {ctx.message.author}", icon_url=ctx.message.author.avatar_url_as(format="png"))
+        
+        await message.edit(content="", embed=embed)
+        await ctx.message.add_reaction("🏓")
+
+
+    @command(name="uptime")
+    async def uptime(self, ctx: Context):
+        pass
+
+
+def setup(bot: Bot):
+    bot.add_cog(Bot_info(bot))
