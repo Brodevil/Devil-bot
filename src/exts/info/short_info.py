5029a29acd4ca05c7f07b6fe11afba491df09030
Brodevil <brodevil89@gmail.com>
2021-09-07 20:25:49 +0530

    ` `

--- src/exts/info/short_info.py
+++ src/exts/info/short_info.py
@@ -44,9 +44,9 @@ class ShortInfo(Cog):
             time = await short_google_search(search)
             
             if country == "UTC":
-                await ctx.send(f"**🕑 Current Time According to UTC : {time.upper()}**")
+                await ctx.send(f"**🕑 Current Time According to UTC : `{time.upper()}`**")
             else:
-                await ctx.send(f"**🕑 Current Time in {country.upper()} : {time.upper()}**")
+                await ctx.send(f"**🕑 Current Time in {country.upper()} : `{time.upper()}`**")
 
 
     @command(name="celcius", aliases=("temprature", "c",))
