99d57dfb4cdde263001cbfd7d93027ef4180f4c8
Brodevil <brodevil89@gmail.com>
2021-11-02 21:04:38 +0530

    Initial commit

--- src/exts/voice/dcactivities.py
+++ src/exts/voice/dcactivities.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 from discord import VoiceChannel
-from discord.ext.commands import Cog, Context, command, cooldown, cooldowns
+from discord.ext.commands import Cog, Context, command
 from discord import Embed
 
 from dcactivity import DCApplication
@@ -10,16 +10,34 @@ from src.bot import Bot
 from src.constants import Colours
 
 
+APPLICATION = {
+    "YOUTUBE" : 1,
+    "CHESS" : 2,
+    "FISHING" : 3,
+    "POKER" : 4,
+    "BETRAYAL" : 5
+}
+
+APPLICATION_EMBED = Embed(
+    title="Application no.",
+    description="",
+    colour=Colours.soft_red
+)
+
+for app, index in APPLICATION.items():
+    APPLICATION_EMBED.add_field(name=app, value=f"`{index}`", inline=False)
+
+
 class ActivityCog(Cog):
     """Custom DCactivity Cog"""
     def __init__(self, bot):
         self.bot = bot
 
-    @command(name="youtube", aliase=["yt"])
-    async def youtube_video(self, ctx: Context, channel: Optional[VoiceChannel] = None):
-        """Command used to watch Youtube together with Friends on Voice Channel"""
-        if not channel:
 
+    @command(name="app", aliase=["app_activity", "application"])
+    async def youtube_video(self, ctx: Context, *, application: Optional[int] = None, channel: Optional[VoiceChannel] = None):
+        """Command used to Play & Watch together"""
+        if not channel:
             if not ctx.author.voice:
                 return await ctx.send('You need to connect to a voice channel first')
 
@@ -31,99 +49,41 @@ class ActivityCog(Cog):
         else:
             _channel = channel
 
+        if not application:
+            if application == 1:
+                image = "https://shorturl.at/gGQS2"
+                application = DCApplication.youtube
+
+            elif application == 2:
+                image = "https://i.pinimg.com/736x/3c/4f/18/3c4f1886e5b1d47f3126703fd20f56b7.jpg"
+                application = DCApplication.chess
+
+            elif application == 3:
+                image = "https://files.brightestgames.com/games/image_large/2021/03/19/fishington-io-315x237.jpg"
+                application = DCApplication.fishing
+
+            elif application == 4:
+                image = ""
+                application = DCApplication.poker
+
+            elif application == 5:
+                application = DCApplication.betrayal
+
+            else:
+                await ctx.send('Invalid Application No.')
+
+
+
         invite = await self.bot.dcactivity.create_invite(
-            _channel, DCApplication.youtube, max_age=86400, max_uses=10)
+            _channel, application, max_age=86400, max_uses=20)
 
         embed = Embed(title="Let's Watch YouTube Together!",
                       color=Colours.soft_red)
         embed.add_field(name="Invitation Link :", value=invite)
-        embed.set_image(url="https://shorturl.at/gGQS2")
+        embed.set_image(url="")
         embed.set_footer(text="This link will expire in 24 hours")
         await ctx.send(embed=embed)
 
-
-    @command(name="poker", aliases=["poker_game", "poker_night"])
-    @cooldown(1, 60, cooldowns.BucketType.user)
-    async def poker_game(self, ctx: Context, channel: Optional[VoiceChannel] = None):
-        """Command used to play poker game with other peoples on Voice Channel"""
-        if not channel:
-
-            if not ctx.author.voice:
-                return await ctx.send('You need to connect to a voice channel first')
-
-            if not isinstance(ctx.author.voice.channel, VoiceChannel):
-                return await ctx.send('This feature is not supported in Stage Channels.')
-
-            _channel = ctx.author.voice.channel
-
-        else:
-            _channel = channel
-
-        invite = await self.bot.dcactivity.create_invite(
-            _channel, DCApplication.poker, max_age=86400, max_uses=10)
-
-        embed = Embed(title="Play Poker Together!", color=Colours.soft_red)
-        embed.add_field(name="Invitation Link :", value=invite)
-        embed.set_image(url="https://shorturl.at/gGQS2")
-        embed.set_footer(text="This link will expire in 24 hours")
-
-        await ctx.send(embed=embed)
-
-
-    @command(name="chess", aliases=["chess_game"])
-    @cooldown(1, 60, cooldowns.BucketType.user)
-    async def chess_game(self, ctx: Context, channel: Optional[VoiceChannel] = None):
-        """Command used to play chess within 2 players on Voice Channel"""
-        if not channel:
-
-            if not ctx.author.voice:
-                return await ctx.send('You need to connect to a voice channel first')
-
-            if not isinstance(ctx.author.voice.channel, VoiceChannel):
-                return await ctx.send('This feature is not supported in Stage Channels.')
-
-            _channel = ctx.author.voice.channel
-
-        else:
-            _channel = channel
-
-        invite = await self.bot.dcactivity.create_invite(
-            _channel, DCApplication.chess, max_age=86400, max_uses=10)
-
-        embed = Embed(title="Play Chess Together!", color=Colours.soft_red)
-        embed.add_field(name="Invitation Link :", value=invite)
-        embed.set_image(url="https://shorturl.at/gGQS2")
-        embed.set_footer(text="This link will expire in 24 hours")
-
-        await ctx.send(embed=embed)
-
-
-    @command(name="fishing", aliases=["hangman_game"])
-    @cooldown(1, 60, cooldowns.BucketType.user)
-    async def hangman_game(self, ctx: Context, channel: Optional[VoiceChannel] = None):
-        """Command used to play hangman within 2 players on Voice Channel"""
-        if not channel:
-
-            if not ctx.author.voice:
-                return await ctx.send('You need to connect to a voice channel first')
-
-            if not isinstance(ctx.author.voice.channel, VoiceChannel):
-                return await ctx.send('This feature is not supported in Stage Channels.')
-
-            _channel = ctx.author.voice.channel
-
-        else:
-            _channel = channel
-
-        invite = await self.bot.dcactivity.create_invite(
-            _channel, DCApplication.fishing, max_age=86400, max_uses=10)
-
-        embed = Embed(title="Play Hangman Together!", color=Colours.soft_red)
-        embed.add_field(name="Invitation Link :", value=invite)
-        embed.set_image(url="https://shorturl.at/gGQS2")
-        embed.set_footer(text="This link will expire in 24 hours")
-
-        await ctx.send(embed=embed)
 
 
 def setup(bot: Bot) -> None:
