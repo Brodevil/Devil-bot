1ed2c43927149e51e23cda5ce0ff5bd80f6b0297
Brodevil <brodevil89@gmail.com>
2021-11-02 20:29:21 +0530

    Store it

--- src/exts/voice/dcactivities.py
+++ src/exts/voice/dcactivities.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 from discord import VoiceChannel
-from discord.ext.commands import Cog, Context, group, command, cooldown, cooldowns
+from discord.ext.commands import Cog, Context, command, cooldown, cooldowns
 from discord import Embed
 
 from dcactivity import DCApplication
@@ -19,7 +19,7 @@ class ActivityCog(Cog):
     async def youtube_video(self, ctx: Context, channel: Optional[VoiceChannel] = None):
         """Command used to watch Youtube together with Friends on Voice Channel"""
         if not channel:
-            
+
             if not ctx.author.voice:
                 return await ctx.send('You need to connect to a voice channel first')
 
@@ -41,12 +41,13 @@ class ActivityCog(Cog):
         embed.set_footer(text="This link will expire in 24 hours")
         await ctx.send(embed=embed)
 
-    @command(name="poker", aliases=["poker_game"])
+
+    @command(name="poker", aliases=["poker_game", "poker_night"])
     @cooldown(1, 60, cooldowns.BucketType.user)
     async def poker_game(self, ctx: Context, channel: Optional[VoiceChannel] = None):
         """Command used to play poker game with other peoples on Voice Channel"""
         if not channel:
-            
+
             if not ctx.author.voice:
                 return await ctx.send('You need to connect to a voice channel first')
 
@@ -67,6 +68,7 @@ class ActivityCog(Cog):
         embed.set_footer(text="This link will expire in 24 hours")
 
         await ctx.send(embed=embed)
+
 
     @command(name="chess", aliases=["chess_game"])
     @cooldown(1, 60, cooldowns.BucketType.user)
@@ -89,6 +91,34 @@ class ActivityCog(Cog):
             _channel, DCApplication.chess, max_age=86400, max_uses=10)
 
         embed = Embed(title="Play Chess Together!", color=Colours.soft_red)
+        embed.add_field(name="Invitation Link :", value=invite)
+        embed.set_image(url="https://shorturl.at/gGQS2")
+        embed.set_footer(text="This link will expire in 24 hours")
+
+        await ctx.send(embed=embed)
+
+
+    @command(name="fishing", aliases=["hangman_game"])
+    @cooldown(1, 60, cooldowns.BucketType.user)
+    async def hangman_game(self, ctx: Context, channel: Optional[VoiceChannel] = None):
+        """Command used to play hangman within 2 players on Voice Channel"""
+        if not channel:
+
+            if not ctx.author.voice:
+                return await ctx.send('You need to connect to a voice channel first')
+
+            if not isinstance(ctx.author.voice.channel, VoiceChannel):
+                return await ctx.send('This feature is not supported in Stage Channels.')
+
+            _channel = ctx.author.voice.channel
+
+        else:
+            _channel = channel
+
+        invite = await self.bot.dcactivity.create_invite(
+            _channel, DCApplication.fishing, max_age=86400, max_uses=10)
+
+        embed = Embed(title="Play Hangman Together!", color=Colours.soft_red)
         embed.add_field(name="Invitation Link :", value=invite)
         embed.set_image(url="https://shorturl.at/gGQS2")
         embed.set_footer(text="This link will expire in 24 hours")
