4928bed55301d618bfec333a25bd329310c2a9ea
Brodevil <brodevil89@gmail.com>
2021-11-02 21:22:53 +0530

    All set

--- src/exts/voice/dcactivities.py
+++ src/exts/voice/dcactivities.py
@@ -20,7 +20,7 @@ APPLICATION = {
 
 APPLICATION_EMBED = Embed(
     title="Application no.",
-    description="",
+    description="```d!app <application no.> [channel]``` \nFollowing are the application no. of several Activity",
     colour=Colours.soft_red
 )
 
@@ -33,9 +33,11 @@ class ActivityCog(Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
     @command(name="app", aliase=["app_activity", "application"])
-    async def youtube_video(self, ctx: Context, *, application: Optional[int] = None, channel: Optional[VoiceChannel] = None):
+    async def youtube_video(self, ctx: Context, *, 
+                            application: Optional[int] = None, 
+                            channel: Optional[VoiceChannel] = None
+                            ) -> None:
         """Command used to Play & Watch together"""
         if not channel:
             if not ctx.author.voice:
@@ -43,47 +45,54 @@ class ActivityCog(Cog):
 
             if not isinstance(ctx.author.voice.channel, VoiceChannel):
                 return await ctx.send('This feature is not supported in Stage Channels.')
-
             _channel = ctx.author.voice.channel
-
+        
         else:
             _channel = channel
 
         if not application:
             if application == 1:
+                statement = "Let's Watch YouTube Together (youtube)!"
                 image = "https://shorturl.at/gGQS2"
                 application = DCApplication.youtube
 
             elif application == 2:
+                statement = "Let's CG 2 DEV (Chess) together!"
                 image = "https://i.pinimg.com/736x/3c/4f/18/3c4f1886e5b1d47f3126703fd20f56b7.jpg"
                 application = DCApplication.chess
 
             elif application == 3:
+                statement = "Let's Play Fishington.io (fishing) Game together!"
                 image = "https://files.brightestgames.com/games/image_large/2021/03/19/fishington-io-315x237.jpg"
                 application = DCApplication.fishing
 
             elif application == 4:
-                image = ""
+                statement = "Let's Play Poker Night (poker) together!"
+                image = "https://upload.wikimedia.org/wikipedia/en/b/bb/Poker-night-at-the-inventory-cover.JPG"
                 application = DCApplication.poker
 
             elif application == 5:
+                statement = "Let's Play Betrayal.io (betrayal) together!"
+                image = "https://i.ytimg.com/vi/Q8KbGO3UqZg/maxresdefault.jpg"
                 application = DCApplication.betrayal
 
             else:
                 await ctx.send('Invalid Application No.')
+                await ctx.send(embed=APPLICATION_EMBED)
+                return
 
-
-
+        else:
+            await ctx.send(embed=APPLICATION_EMBED)
+            return
+        
         invite = await self.bot.dcactivity.create_invite(
             _channel, application, max_age=86400, max_uses=20)
 
-        embed = Embed(title="Let's Watch YouTube Together!",
-                      color=Colours.soft_red)
+        embed = Embed(title=statement, color=Colours.soft_red)
         embed.add_field(name="Invitation Link :", value=invite)
-        embed.set_image(url="")
+        embed.set_image(url=image)
         embed.set_footer(text="This link will expire in 24 hours")
         await ctx.send(embed=embed)
-
 
 
 def setup(bot: Bot) -> None:
